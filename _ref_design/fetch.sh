57a37f3c9179667cdc0d942d813e76b47e2a2046
louiscklaw <louiscklaw@gmail.com>
2019-12-25 01:45:34 +0800

    update,

--- _ref_design/fetch.sh
+++ _ref_design/fetch.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 
-git submodule add --depth=10 https://github.com/anzerr/pc.case.git
 git submodule add --depth=10 https://github.com/aliuliu121/NeonSFF
 git submodule add --depth=10 https://github.com/amirmashal/Cube_Mini-ITX
 git submodule add --depth=10 https://github.com/amirmashal/MicroATX-BoxChassis
 git submodule add --depth=10 https://github.com/amirmashal/ProMini_ITX_Case
+git submodule add --depth=10 https://github.com/anzerr/pc.case.git
 git submodule add --depth=10 https://github.com/berserkwarwolf/OpenCase
 git submodule add --depth=10 https://github.com/brandonmarino/Mini-itx-Custom-Case-3DModel
 git submodule add --depth=10 https://github.com/cleeus/woodnas
 git submodule add --depth=10 https://github.com/cokeeffekt/yasff
 git submodule add --depth=10 https://github.com/NanyiJiang/Skyscraper
 git submodule add --depth=10 https://github.com/phkahler/mellori_ITX
+git submodule add --depth=10 https://github.com/phkahler/mellori_ITX.git
 git submodule add --depth=10 https://github.com/simonvanderveldt/sfx-openbeam
 git submodule add --depth=10 https://github.com/sprucegum/honeyitx
 git submodule add --depth=10 https://github.com/tahnok/LACC
