129a3800c81313481c5efc1eacfda701af7dce7d
louiscklaw <louiscklaw@gmail.com>
2020-01-25 14:56:41 +0800

    update fetch.sh,

---
+++ rpi-case/fetch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+https://github.com/PiSugar/pisugar-case-pihat-cap.git
+https://github.com/krotik/spieljunge.git
+https://github.com/PolyVection/RPI-ZERO-case.git
+https://github.com/AltJ/raspberry-pi-case.git
+https://github.com/mkellsy/rpi-case-model.git
+
+https://github.com/DesignSparkrs/pi-cluster-case.git
+https://github.com/raspberrypilearning/3d-printed-astro-pi-flight-case.git
+https://github.com/raspilot/3D-Printed-Case.git
+https://github.com/ShacharWeis/Pi-Thermal-Printer-Camera.git
+https://github.com/krotik/spieljunge.git
+https://github.com/stsb11/SB-Pi-Pi-Case
+
+
+git submodule add --depth=10 https://github.com/JimKnopfIoT/nymea-rpi4-case.git
